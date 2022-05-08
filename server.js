84514285bb2b5b5d5d4587be3d9041441cc8246a
RafidMuhymin <rafidmuhymin@gmail.com>
2022-05-08 12:23:19 +0600

    added cors middleware

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+import cors from "cors";
 import express from "express";
 import { MongoClient } from "mongodb";
 
-const uri =
-  "mongodb+srv://admin_soft_hard_system:yHb8P1bSRzhLgghx@cluster0.avzmu.mongodb.net/spark-plug?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri);
-
 const app = express(),
-  port = process.env.PORT || 8080;
+  port = process.env.PORT || 8080,
+  uri =
+    "mongodb+srv://admin_soft_hard_system:yHb8P1bSRzhLgghx@cluster0.avzmu.mongodb.net/spark-plug?retryWrites=true&w=majority",
+  client = new MongoClient(uri);
+
+app.use(cors());
 
 app.get("/cars", async function (req, res, next) {
   try {
